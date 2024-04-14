be76567d1b1430b36ac91c64d17e7bd0657630f7
Dirk Breeuwer <breeuwer@google.com>
2024-04-14 08:52:28 +0000

    htc add old assignments

---
+++ core-programming/course-how-to-code-simple-data-ubc/assignments/01a_beginning_student_language/booleans.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname booleans) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;(<= (+ 1 1) 2)
+
+;(string=? "foo" "foo")
+
+(if (< 1 2) "t" "f")
